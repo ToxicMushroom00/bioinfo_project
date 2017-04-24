470d0450d05de050441b919c67783b103ea1f3c6
DTrizzy <yosoup@outlook.com>
2017-04-24 16:34:46 -0700

    finish filling out dict of DNA codon -> amino acid translation table

--- parsing.py
+++ parsing.py
@@ -14,14 +14,21 @@ codons: {
     "lle": ["ATT", "ATC", "ATA"],
     "Met": ["ATG"]
     "Val": ["GTT", "GTC", "GTA", "GTG"],
-    "Ser": ["TCT", "TCC", "TCA", "TCG"],
+    "Ser": ["TCT", "TCC", "TCA", "TCG", "AGT", "AGC"],
     "Pro": ["CCT", "CCC", "CCA", "CCG"],
     "Thr": ["ACT", "ACC", "ACA", "ACG"],
     "Ala": ["GCT", "GCC", "GCA", "GCG"],
     "Tyr": ["TAT", "TAC"],
     "His": ["CAT", "CAC"],
     "Gin": ["CAA", "CAG"],
-    "Asn": [""]
+    "Asn": ["AAT", "AAC"],
+    "Lys": ["AAA", "AAG"],
+    "Asp": ["GAT", "GAC"],
+    "Glu": ["GAA", "GAG"],
+    "Cys": ["TGT", "TGC"],
+    "Trp": ["TGG"],
+    "Arg": ["CGT", "CGC", "CGA", "CGG", "AGA", "AGG"],
+    "Gly": ["GGT", "GGC", "GGA", "GGG"]
 }
 
 def findAmino(codon):
