42320f1470ed5075c70faee81b5bc3255f1503d9
DTrizzy <yosoup@outlook.com>
2017-04-25 21:37:45 -0700

    now accurately counts codon usage (for the whole fasta file); fix a bug where codon were not properly uppercase before comparing; fix a bug where comparing string to list

--- parsing.py
+++ parsing.py
@@ -1,8 +1,9 @@
 import sys
+import itertools
 
 # ! make sure to make all codons uppercase when comparing!
 codons = {
-    "start": ["ATG"],
+    # "start": ["AUG"], #no start codon in the fasta file?
     "stop": ["TAA", "TAG", "TGA"],
     "Phe": ["TTT", "TTC"],
     "Leu": ["TTA", "TTG", "CTT", "CTC", "CTA", "CTG"],
@@ -26,29 +27,65 @@ codons = {
     "Gly": ["GGT", "GGC", "GGA", "GGG"]
 }
 
+
 # Find the Amino Acid that the given codon codes for
-def findAmino(codon):
+def find_amino(codon):
     for key in codons:
         for value in codons.get(key):
             if value == codon:
-                print(key)
+                return key
 
-# file = "NC_005363_HEG_NT.fasta"
-file = "test.fasta"
+
+# I use this method below to help group the input strings into 3s
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return itertools.zip_longest(*args, fillvalue=fillvalue)
+
+
+start_count = 0
+carrot_count = 0
+file = "NC_005363_HEG_NT.fasta"
+# file = "test.fasta"
+codon_parsed = []
 with open(file) as curr:
+    carrot_line = ''
     for line in curr.readlines():
         # look for carrot ">" line
         if line[0] == '>':
-            # if found ">" line, skip
+            # if found ">" line, save it and skip
+            carrot_line = line.replace("\n", "")
+            carrot_count += 1
             continue
         else:  # now we're working with the gene line!
-            # look for start codon
-            if line[:3] == "lol":
-                print("found start codon!")
-                print(line[:3])
-            # filter start codon out
-            filtered_line = line[3:]
+            line = line.upper()
+
+            # # look for start codon
+            # # if line[:3] == "lol": # using test data
+            # if line[:3] == ''.join(codons["start"]):  # using actual data
+            #     start_count += 1
+            #     # print(line[:3])
+            # # filter start codon out
+            # # filtered_line = line[3:]
+            filtered_line = line
+
+            # print(filtered_line)
+            # use filtered_line to perform analysis from here on out
+            codon_parsed.append(carrot_line)
+            for chunk in grouper(filtered_line, 3, ''):
+                codon_parsed.append(''.join(chunk))
 
 
+codon_count = {}
+for codon in codon_parsed:
+    result = find_amino(codon)
+    if result in codon_count:
+        codon_count[result] += 1
+    else:
+        codon_count[result] = 1
 
+print(start_count)
+print(carrot_count)
+print(codon_count)
 
